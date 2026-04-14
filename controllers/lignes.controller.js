5e6aafb282f3dbba08581047916438aa19f54ab5
khalilba <khalilbenritha@gmail.com>
2026-04-14 11:48:38 +0100

    Add line management handlers to lignes.controller.js

---
+++ controllers/lignes.controller.js
@@ -0,0 +1,29 @@
+'use strict';
+
+class LineController {
+    constructor() {
+        // Initialization code here
+    }
+
+    // Handler to create a line
+    createLine(req, res) {
+        // Logic to create a line
+    }
+
+    // Handler to get a line
+    getLine(req, res) {
+        // Logic to get a line by ID
+    }
+
+    // Handler to update a line
+    updateLine(req, res) {
+        // Logic to update a line by ID
+    }
+
+    // Handler to delete a line
+    deleteLine(req, res) {
+        // Logic to delete a line by ID
+    }
+}
+
+module.exports = new LineController();
