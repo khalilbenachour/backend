9cba5b6d5c3ae3239839f193047bad4fa840a6d5
khalilba <khalilbenritha@gmail.com>
2026-04-14 11:48:12 +0100

    Create roles.controller.js with user roles and agencies handlers

---
+++ controllers/roles.controller.js
@@ -0,0 +1,27 @@
+// roles.controller.js
+
+// Handler for user roles
+export const getUserRoles = (req, res) => {
+    // Logic to get user roles
+};
+
+export const createUserRole = (req, res) => {
+    // Logic to create a new user role
+};
+
+export const updateUserRole = (req, res) => {
+    // Logic to update an existing user role
+};
+
+// Handler for agencies
+export const getAgencies = (req, res) => {
+    // Logic to get agencies
+};
+
+export const createAgency = (req, res) => {
+    // Logic to create a new agency
+};
+
+export const updateAgency = (req, res) => {
+    // Logic to update an existing agency
+};
