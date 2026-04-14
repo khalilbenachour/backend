e6fa54e2d93be72dcf7f50185a63c4a383e4bd84
khalilba <khalilbenritha@gmail.com>
2026-04-14 11:49:30 +0100

    Create request validation middleware

---
+++ middlewares/validate.middleware.js
@@ -0,0 +1,14 @@
+// validate.middleware.js
+
+const { validationResult } = require('express-validator');
+
+// Middleware for validating request data
+const validateRequest = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+};
+
+module.exports = validateRequest;
